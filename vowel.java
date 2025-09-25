c6adf5c2dad88208dab96b4207771c3dc7405a4a
unknown <nithinpathayathodi@gmail.com>
2025-09-26 00:01:18 +0530

    files added

---
+++ vowel.java
@@ -0,0 +1,106 @@
+// public class test{
+// public static void main(String args[]){
+// System.out.print("hello,java");
+
+// }
+// }
+import java.util.Scanner;
+// public class calc{
+
+// public static void main(String args[]){
+// sum();
+// difference();
+// product();
+// quotient();
+// }
+
+// static void sum(){
+// int a,b,sum;
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter two numbers to get sum");
+// a = sc.nextInt();
+// b =sc.nextInt();
+
+// sum = a+b;
+// System.out.println(sum);
+// }
+
+// static void difference(){
+// int a,b,sub;
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter two numbers to know difference");
+// a = sc.nextInt();
+// b =sc.nextInt();
+
+// sub = a-b;
+// System.out.println(sub);
+// }
+
+// static void product(){
+// int a,b,product;
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter two numbers to know difference");
+// a = sc.nextInt();
+// b =sc.nextInt();
+
+// product = a*b;
+// System.out.println(product);
+// }
+
+// static void quotient(){
+// int a,b,quo;
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter two numbers to know difference");
+// a = sc.nextInt();
+// b =sc.nextInt();
+
+// quo = a/b;
+// System.out.println(quo);
+// }
+
+// }
+
+// public class swap{
+// public static void main(String args[]){
+// int a = 10;
+// int b =20;
+// b = b-a;
+// a = a+b;
+// System.out.println("a is: "+a);
+// System.out.println("b is: "+b);
+
+// }
+
+
+// }
+// import java.util.Scanner;
+// public class leap{
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter a year to check if it is leap year");
+// int year = sc.nextInt();
+// if(year%4==0 && year !=100 || year%400 ==0){
+//     System.out.println(year+" is Leap year");
+// }else{
+//     System.out.println(year+"is not leap year");
+// }
+
+//     }
+// }
+// import java.util.Scanner;
+// public class vowel{
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.println("check if character is vowel or consonant");   
+
+// char ch =Character.toLowerCase(sc.next().charAt(0));
+// String vowels="aeiou";
+// if(vowels.contains(String.valueOf(ch))){
+//     System.out.println(ch+" is a vowel");
+
+// }else{
+//     System.out.println(ch+" is a consonant");
+// }
+
+//     }
+// }
